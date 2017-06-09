b2967cf23bab70fd3a7d15fb6925e6017a8569f4
iyke <nderiony95@gmail.com>
2017-06-09 17:56:46 +0300

    add answer model

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -5,6 +5,15 @@ export default Ember.Component.extend({
   actions: {
     questionFormShow() {
       this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        query: this.get('query'),
+        author: this.get('author'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
     }
   }
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    update(rental, params){
+      this.sendAction('update', question, params);
+    }
+  }
 });

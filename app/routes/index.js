0d2b09773343bd6957ea9be37e08e2aa118ea9bb
iyke <nderiony95@gmail.com>
2017-06-09 12:49:44 +0300

    add a model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  question: "What is the meaning of life?",
+  author: "John wayne"
+},{
+  id: 2,
+  question: "How manypeople are there on earth?",
+  author: "Paul"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return rentals;
+  },
 });

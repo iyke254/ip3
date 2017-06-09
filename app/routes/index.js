81d5051de12ca842d7b1a7cece0bd9840f70d9d6
iyke <nderiony95@gmail.com>
2017-06-09 14:28:07 +0300

    add firebase to the project and updated the index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  id: 1,
-  query: "What is the meaning of life?",
-  author: "John wayne"
-},{
-  id: 2,
-  query: "How many people are there on earth?",
-  author: "Paul"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
 });

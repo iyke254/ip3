c2623dddcc592525c94e6b7446ff378eaa5429c7
iyke <nderiony95@gmail.com>
2017-06-09 13:03:36 +0300

    add the question model and defined its attributes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 var questions = [{
   id: 1,
-  question: "What is the meaning of life?",
+  query: "What is the meaning of life?",
   author: "John wayne"
 },{
   id: 2,
-  question: "How manypeople are there on earth?",
+  query: "How many people are there on earth?",
   author: "Paul"
 }];
 
 export default Ember.Route.extend({
   model() {
-    return rentals;
+    return questions;
   },
 });
